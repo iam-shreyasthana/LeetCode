0eb5ed14edbdcd27c91ba271417c69d97c3d6909
Shrey Asthana <shreyasthana545@gmail.com>
2022-09-11 05:33:16 +0530

    three consecutive odds questions added

---
+++ Three-Consecutive-Odds/Three-Consecutive-Odds.cpp
@@ -0,0 +1,17 @@
+/*
+    #1550 Three Consecutive Odds.....
+    https://leetcode.com/problems/three-consecutive-odds/
+*/
+
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        for(int i = 2; i < arr.size(); i++){
+            if(arr[i]%2 != 0 && arr[i-1]%2 != 0 && arr[i-2]%2 != 0){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
