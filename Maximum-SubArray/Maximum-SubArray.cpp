ba4fc6cd3f25e846b2175438907bb6c79be32dc0
Shrey Asthana <shreyasthana545@gmail.com>
2022-09-13 08:18:45 +0530

    added maximum subarray

---
+++ Maximum-SubArray/Maximum-SubArray.cpp
@@ -0,0 +1,28 @@
+/*
+    #53. Maximum Subarray
+    https://leetcode.com/problems/maximum-subarray/
+*/
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int result = 0;
+        int findMax = INT_MIN;
+        
+        for(int i = 0; i < nums.size(); i++){
+            
+            result += nums[i];
+            
+            if(findMax < result){
+                findMax = result;
+            }
+            
+            if(result < 0){
+                result = 0;
+            }
+            
+        }
+        
+        return findMax;
+    }
+};
